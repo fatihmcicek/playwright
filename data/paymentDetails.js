5db5cf8fd2f328484779b67c0dcc64a1b598738f
Fatih Mehmet Çiçek <fatihmcicek5@gmail.com>
2024-10-26 12:31:13 +0300

    payment page and delivery details

---
+++ data/paymentDetails.js
@@ -0,0 +1,6 @@
+export const paymentDetails = {
+    owner: "Fatih Mehmet ÇİÇEK",
+    number: "1234123412341234",
+    validUntil: "12/47",
+    cvc: "123",
+}
---
+++ page-objects/PaymentPage.js
@@ -0,0 +1,59 @@
+import { expect } from "@playwright/test"
+
+export class PaymentPage {
+    constructor(page) {
+        this.page = page
+
+        this.discountCode = page.frameLocator('[data-qa="active-discount-container"]')
+            .locator('[data-qa="discount-code"]')
+        this.discountInput = page.getByPlaceholder('Discount code')
+        this.activateDiscountButton = page.locator('[data-qa="submit-discount-button"]')
+        this.totalValue = page.locator('[data-qa="total-value"]')
+        this.discountedValue = page.locator('[data-qa="total-with-discount-value"]')
+        this.discountActiveMessage = page.locator('[data-qa="discount-active-message"]')
+
+        this.creditCardOwnerInput = page.getByPlaceholder("Credit card owner")
+        this.creditCardNumberInput = page.getByPlaceholder("Credit card number")
+        this.creditCardValidUntilInput = page.getByPlaceholder("Valid until")
+        this.creditCardCvcInput = page.getByPlaceholder("Credit card CVC")
+    }
+
+    activateDiscount = async () => {
+        await this.discountCode.waitFor()
+        const code = await this.discountCode.innerText()
+        await this.discountInput.waitFor()
+        await this.discountInput.fill(code)
+        await expect(this.discountInput).toHaveValue(code)
+
+        expect(await this.discountedValue.isVisible()).toBe(false)
+        expect(await this.discountActiveMessage.isVisible()).toBe(false)
+        await this.activateDiscountButton.waitFor()
+        await this.activateDiscountButton.click()
+
+        await this.discountActiveMessage.waitFor()
+
+        await this.discountedValue.waitFor()
+        const discountValueText = await this.discountedValue.innerText() // "345$"
+        const discountValueOnlyStringNumber = discountValueText.replace("$", "")
+        const discountValueNumber = parseInt(discountValueOnlyStringNumber, 10)
+
+        await this.totalValue.waitFor()
+        const totalValueText = await this.totalValue.innerText() // "345$"
+        const totalValueOnlyStringNumber = totalValueText.replace("$", "")
+        const totalValueNumber = parseInt(totalValueOnlyStringNumber, 10)
+        expect(discountValueNumber).toBeLessThan(totalValueNumber)
+
+    }
+
+    fillPaymentDetails = async (paymentDetails) => {
+        await this.creditCardOwnerInput.waitFor()
+        await this.creditCardOwnerInput.fill(paymentDetails.owner)
+        await this.creditCardNumberInput.waitFor()
+        await this.creditCardNumberInput.fill(paymentDetails.number)
+        await this.creditCardValidUntilInput.waitFor()
+        await this.creditCardValidUntilInput.fill(paymentDetails.validUntil)
+        await this.creditCardCvcInput.waitFor()
+        await this.creditCardCvcInput.fill(paymentDetails.cvc)
+        await this.page.pause()
+    }
+}
